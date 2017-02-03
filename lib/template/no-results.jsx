d69c457cf959cb992d2b3f412c89a872d169bfbf
Barthelemy Dagenais <bart@resulto.ca>
2017-02-03 10:41:21 -0500

    Added InnerHTML in noResults.

--- lib/template/no-results.jsx
+++ lib/template/no-results.jsx
@@ -1,9 +1,10 @@
 module.exports = function(h, that) {
+    var innerNoResults = that.display("noResults");
     if (that.count==0)
    return <tr class="VueTables__no-results">
   <td class="text-center"
-  colspan={that.allColumns.length}>
-  {that.display('noResults')}
+  colspan={that.allColumns.length}
+  domPropsInnerHTML={innerNoResults}>
   </td>
   </tr>
 }
